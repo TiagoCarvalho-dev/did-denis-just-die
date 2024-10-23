6daac01e910de1faa6c90df9ede91a663f9c4f82
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2024-10-23 20:39:28 -0300

    Basic logic ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,89 @@
 import "./styles.css"
+import classicImg from "./images/classic.jpg"
+import ezImg from "./images/ez.png"
 
-console.log("Hello World");
+const mainContent = document.querySelector(".main-content");
+
+const resetButton = document.createElement("button");
+resetButton.textContent = "RESET";
+
+const deathYesButton = document.createElement("button");
+deathYesButton.textContent = "YES";
+
+const deathNoButton = document.createElement("button");
+deathNoButton.textContent = "NO";
+
+function buildMainPage() {
+  mainContent.appendChild(deathYesButton);
+  mainContent.appendChild(deathNoButton);
+}
+
+function clearMainContent() {
+  while(mainContent.firstChild) {
+    mainContent.removeChild(mainContent.lastChild);
+  }
+}
+
+resetButton.addEventListener("click", () => {
+  clearMainContent();
+  buildMainPage();
+});
+
+deathYesButton.addEventListener("click", () => {
+  clearMainContent();
+
+  const howDidDenisDieTitle = document.createElement("h2");
+  howDidDenisDieTitle.textContent = "HOW DID HE DIE?";
+
+  mainContent.appendChild(howDidDenisDieTitle);
+
+  const deathCauseInput = document.createElement("input");
+  deathCauseInput.setAttribute("type", "text");
+
+  mainContent.append(deathCauseInput);
+
+  const submitButton = document.createElement("button");
+  mainContent.appendChild(submitButton);
+
+  submitButton.addEventListener("click", () => {
+    clearMainContent();
+
+    const vpnSaveMessage = document.createElement("h2");
+    vpnSaveMessage.textContent = "WOULDN'T HAVE DIED USING A VPN";
+
+    const classic = document.createElement("img");
+    classic.src = classicImg;
+    classic.setAttribute("width", "90px");
+    classic.setAttribute("height", "auto");
+
+    const messageDiv = document.createElement("div");
+
+    messageDiv.appendChild(vpnSaveMessage);
+    messageDiv.appendChild(classic);
+
+    mainContent.appendChild(messageDiv);
+    mainContent.appendChild(resetButton);
+  });
+});
+
+deathNoButton.addEventListener("click", () => {
+  clearMainContent();
+
+  const denisGamingMessage = document.createElement("h2");
+  denisGamingMessage.textContent = "HE IS GAMING";
+
+  const ez = document.createElement("img");
+  ez.src = ezImg;
+  ez.setAttribute("width", "50px");
+  ez.setAttribute("height", "auto");
+
+  const messageDiv = document.createElement("div");
+
+  messageDiv.appendChild(denisGamingMessage);
+  messageDiv.appendChild(ez);
+
+  mainContent.appendChild(messageDiv);
+  mainContent.appendChild(resetButton);
+});
+
+buildMainPage();
